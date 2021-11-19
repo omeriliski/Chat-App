d95dda39dd37065ccdb0a5500c88d79935c9f22b
Ömer Iliski <omeriliski06@gmail.com>
2021-11-19 21:57:25 +0100

    running synchronously

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,22 @@
 <template>
     <div>
         <app-header/>
+        <div>
+            <h6 v-for="message in messageList" :key="message"><span>{{message.user.username}}:</span> {{message.text}}</h6>
+        </div>
         <div class="socket">
             <input class="input-message" type="text" @keydown.enter="SEND_MESSAGE">
         </div>
-        <h6>{{messageText}}</h6>
     </div>
 </template>
 
 <script>
 import appHeader from '@/components/appHeader'
+import { mapGetters } from 'vuex';
 export default {
     data() {
         return {
-            messageText:""
+            messageList:[],
         }
     },
     components:{
@@ -21,19 +24,36 @@ export default {
     },
     mounted() {
         this.$socket.on("MESSAGE",this.GET_MESSAGE);
+        this.$appAxios.get(`/messages`)
+            .then(res=>{
+                console.log("messages",res.data);
+                this.messageList=res.data;
+            })
     },
     methods: {
         // WELCOME_MESSAGE(data){
         //     console.log(data);
         // },
         GET_MESSAGE(msj){
-            this.messageText=msj;
+            this.messageList.push(msj);
+            console.log(this.messageList);
         },
         SEND_MESSAGE(e){
-            this.$socket.emit("MESSAGE", e.target.value);
+            let messageData={
+                "text":e.target.value,
+                "user":this._getCurrentUser
+            }
+            this.$socket.emit("MESSAGE", messageData);
+            this.$appAxios.post("/messages",messageData)
+                .then(res=>{
+                    console.log(res.data);
+                })
             e.target.value="";
         }
     },
+    computed:{
+        ...mapGetters(["_getCurrentUser"])
+    }
 }
 </script>
 
