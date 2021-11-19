1121d6fb39ce3bb7590f8d775005bcfe1208cba6
Ömer Iliski <omeriliski06@gmail.com>
2021-11-19 20:37:20 +0100

    added message methods

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue';
-import router from "./router"
-import store from './store'
-import "@/assets/style.css"
-import {appAxios} from './utils/appAxios'
+import router from "./router";
+import store from './store';
+import "@/assets/style.css";
+import {appAxios} from './utils/appAxios';
+import io from "socket.io-client";
 
+const socket=io("http://localhost:2021");
 const app=createApp(App);
 app.use(router);
 app.use(store);
 app.config.globalProperties.$appAxios = appAxios;
+app.config.globalProperties.$socket=socket;
 app.mount('#app');
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,44 @@
 <template>
     <div>
         <app-header/>
+        <div class="socket">
+            <input class="input-message" type="text" @keydown.enter="SEND_MESSAGE">
+        </div>
+        <h6>{{messageText}}</h6>
     </div>
 </template>
 
 <script>
 import appHeader from '@/components/appHeader'
 export default {
+    data() {
+        return {
+            messageText:""
+        }
+    },
     components:{
         appHeader
-    }
+    },
+    mounted() {
+        this.$socket.on("MESSAGE",this.GET_MESSAGE);
+    },
+    methods: {
+        // WELCOME_MESSAGE(data){
+        //     console.log(data);
+        // },
+        GET_MESSAGE(msj){
+            this.messageText=msj;
+        },
+        SEND_MESSAGE(e){
+            this.$socket.emit("MESSAGE", e.target.value);
+            e.target.value="";
+        }
+    },
 }
 </script>
+
+<style>
+    .input-message{
+        background-color: lightblue;
+    }
+</style>
